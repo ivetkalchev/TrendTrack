b7e73aef2791b0125f92e3128d7792f22c409389
i526066 <526066@student.fontys.nl>
2024-11-16 00:10:29 +0100

    SweetAlert2

--- src/components/FabricControlPanel.jsx
+++ src/components/FabricControlPanel.jsx
@@ -2,15 +2,30 @@ import React, { useState } from 'react';
 import './FabricControlPanel.css';
 import EditProductForm from './EditFabric';
 import { FaEdit, FaTrash } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const FabricControlPanel = ({ product, onDelete, onUpdate }) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleDelete = () => {
-    const confirmDelete = window.confirm('Are you sure you want to delete this fabric?');
-    if (confirmDelete) {
-      onDelete(product.id);
-    }
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onDelete(product.id);
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your fabric has been deleted.",
+          icon: "success"
+        });
+      }
+    });
   };
 
   const handleEdit = (updatedProduct) => {
@@ -20,14 +35,16 @@ const FabricControlPanel = ({ product, onDelete, onUpdate }) => {
 
   return (
     <div className="product-admin-item">
-      <h3 className="product-title">{product.name}</h3>
-      <div className="button-container">
-        <button onClick={() => setIsEditing(true)} className="edit-button">
-          <FaEdit /> Edit
-        </button>
-        <button onClick={handleDelete} className="delete-button">
-          <FaTrash /> Delete
-        </button>
+      <div className="product-header">
+        <h3 className="product-title">{product.name}</h3>
+        <div className="button-container">
+          <button onClick={() => setIsEditing(true)} className="edit-button">
+            <FaEdit />
+          </button>
+          <button onClick={handleDelete} className="delete-button">
+            <FaTrash />
+          </button>
+        </div>
       </div>
       {isEditing && (
         <EditProductForm
