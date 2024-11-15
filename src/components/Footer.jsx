6a6cfce1080e19d2de0ccabc634dda11ef394ef0
i526066 <526066@student.fontys.nl>
2024-11-15 18:48:06 +0100

    footer style changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import './Footer.css';
-import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa'; 
+import { FaInstagram, FaLinkedin, FaTimes } from 'react-icons/fa'; 
+import { FiSearch } from 'react-icons/fi';
+import needleIcon from "../assets/img/needle.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-left">
-        <p>© 2024 TrendTrack. All rights reserved.</p>
-      </div>
-      <div className="footer-right">
-        <a href="https://www.facebook.com" target="_blank" rel="noreferrer" aria-label="Facebook">
-          <FaFacebookF />
-        </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noreferrer" aria-label="Instagram">
-          <FaInstagram />
-        </a>
-        <a href="https://www.twitter.com" target="_blank" rel="noreferrer" aria-label="Twitter">
-          <FaTwitter />
-        </a>
+      <div className="footer-content">
+        <div className="footer-header">
+          <div className="header-text">
+            <h2>Wish to Contact Us?</h2>
+            <p className="footer-tagline">Silky flow filled with fashion</p>
+          </div>
+          <img src={needleIcon} alt="Needle icon" className="needle-icon" />
+        </div>
+        <hr className="footer-divider" />
+        <div className="footer-details">
+          <div className="footer-contact">
+            <p>Eindhoven, Netherlands</p>
+            <a href="tel:+000000000">+00 000 0000 000</a>
+          </div>
+          <div className="footer-social">
+            <a href="https://www.instagram.com" target="_blank" rel="noreferrer" aria-label="Instagram">
+              <FaInstagram />
+            </a>
+            <a href="https://www.linkedin.com" target="_blank" rel="noreferrer" aria-label="LinkedIn">
+              <FaLinkedin />
+            </a>
+            <a href="#" aria-label="Close">
+              <FaTimes />
+            </a>
+          </div>
+        </div>
+        <p className="footer-copyright">© 2024 TrendTrack. All rights reserved.</p>
       </div>
     </footer>
   );
