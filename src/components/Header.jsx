47de97e56674cf7c4132d60c0151152453c5fdeb
i526066 <526066@student.fontys.nl>
2024-12-05 15:02:00 +0100

    nav bar login-logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import TokenManager from "../services/tokenManager";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setIsLoggedIn(!!TokenManager.getAccessToken());
+  }, []);
+
+  const handleLogout = () => {
+    TokenManager.clear();
+    setIsLoggedIn(false);
+  };
+
   return (
     <header>
       <Link to="/" className="header-title">
         <h1>TrendTrack</h1>
       </Link>
       <nav>
-        <Link to="/fabric-management">Fabric Managment</Link>
-        <Link to="/user-management">User Managment</Link>
+        <Link to="/fabric-management">Fabric Management</Link>
+        <Link to="/user-management">User Management</Link>
         <Link to="/orders">Orders</Link>
         <Link to="/statistics">Statistics</Link>
         <Link to="/catalogue">Catalogue</Link>
         <Link to="/cart">Cart</Link>
       </nav>
-      <Link to="/authentication" className="login-button">Login</Link>
+      {isLoggedIn ? (
+        <button onClick={handleLogout} className="logout-button">
+          Logout
+        </button>
+      ) : (
+        <Link to="/authentication" className="login-button">
+          Login
+        </Link>
+      )}
     </header>
   );
 };
